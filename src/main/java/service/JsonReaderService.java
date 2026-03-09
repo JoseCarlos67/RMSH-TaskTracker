0d96a2c6a87d255189a143e49dc43ab204f80090
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-03-08 22:03:40 -0300

    refactor: utilização do BufferedReader na leitura do .json

--- src/main/java/service/JsonReaderService.java
+++ src/main/java/service/JsonReaderService.java
@@ -4,18 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import model.Task;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class JsonReaderService {
 
-  public Task newOjbjectFromFileJson() throws IOException {
+  public Task newOjbjectFromFileJson() {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.registerModule(new JavaTimeModule());
 
-    Task task = objectMapper.readValue(new File("/home/carlos/Development/Java/RoadMapSh/RMSH-TaskTracker/src/main/java/files/tasks.json"), Task.class);
-
-    return task;
+    try {
+      Task task = objectMapper.readValue(new BufferedReader(new FileReader("src/main/java/files/tasks.json")), Task.class);
+      return  task;
+    } catch (IOException e) {
+      System.out.println("Erro na leitura do arquivo .json: " + e.getMessage());
+    }
+    return null;
   }
 
 }
